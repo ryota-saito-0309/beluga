ac90b4ddcf27a618a606e02f029399253b87385f
Ryota Saito <sa110.instagram@gmail.com>
2020-06-17 17:40:01 +0900

    注文確定ページの表示を更新しました。

--- src/components/checkout/Confirm.js
+++ src/components/checkout/Confirm.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React from "react";
 import { withRouter } from "react-router-dom";
 
-import PageWrapper from '../ui/PageWrapper';
-import Paper from '@material-ui/core/Paper';
+import PageWrapper from "../ui/PageWrapper";
+import Paper from "@material-ui/core/Paper";
 
-const Confirm = props => (
-  <PageWrapper>
+function Success(props) {
+  return (
     <Paper style={{ padding: "40px", minHeight: "500px" }}>
-      <h2 style={{ marginTop: 0, fontWeight: 600 }}>Thank you for your purchase!</h2>
-      <p>A confirmation email has been sent to <b>{props.location.state.order.email}</b>.</p>
-      <p>Order ID: {props.location.state.order.id.split("_")[1]}</p>
+      <h2 style={{ marginTop: 0, fontWeight: 600 }}>
+        Thank you for your purchase!
+      </h2>
+      <p>
+        A confirmation email has been sent to <b>{props.email}</b>.
+      </p>
+      <p>Order ID: {props.id.split("_")[1]}</p>
     </Paper>
+  );
+}
+
+function Failure() {
+  return (
+    <Paper style={{ padding: "40px", minHeight: "500px" }}>
+      <h2 style={{ marginTop: 0, fontWeight: 600 }}>
+        支払い処理が失敗しました。
+      </h2>
+    </Paper>
+  );
+}
+const Confirm = (props) => (
+  <PageWrapper>
+    {console.log(props.location.state.order.email != null)}
+    {props.location.state.order.email != null && (
+      <Success
+        email={props.location.state.order.email}
+        id={props.location.state.order.id}
+      />
+    )}
+    {props.location.state.order.email == null && <Failure />}
   </PageWrapper>
 );
 export default withRouter(Confirm);
